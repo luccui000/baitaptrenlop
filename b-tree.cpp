6ccb38546bd9b1afb1dd80d7d5be846be23c52b6
luccui000 <lucui2k@gmail.com>
2020-07-08 20:24:20 +0700

    Them B-Tree

--- b-tree.cpp
+++ b-tree.cpp
@@ -7,19 +7,51 @@ struct Node {
 	int numTree;
 	int branch[M-1];
 	// M la so bac 
+	struct Node*[M];
 };
 struct Node* NodePtr;
 NodePtr *root;
-enum KeyValue { };
+enum KeyStatus { Duplicate, SearchFailure, Success, Insert, LessKey };
 // Them phan tu vao B-Tree
 NodePtr father(NodePtr node) {
 }
-void split(NodePtr node, int n, NodePtr newNode, NodePtr* node2, int* m) {
-
-}
-int NodeSearch(NodePtr node, int m) {
+void copy(NodePtr node, int first, int last, NodePtr node2) {
+	for(int i = first; i < last; i++) 
+		node2->branch[i-first] = node->branch[i];
+	for(int i = first; i < last; i++) {
+		node2->con[i-first] = node-branch[i];
+	node2->numTree = last-first + 2;
 }
 void insNode(NodePtr node, int n, NodePtr newNode, int p) {
+
+}
+void split(NodePtr node, int n, NodePtr newNode, int pos, NodePtr* node2, int* m) {
+	NodePtr p = getNode();
+	if(pos > N / 2) {
+		copy(node, N / 2 + 1, ORDER - 2, p);
+		insNode(p, n, pos - N/2-1)
+		node->numTree = N / 2 + 1;
+		*m = node->branch[N/2];
+		*node2 = p;
+		return;
+	} 
+	if(pos == N / 2){
+		copy(node, N / 2, ORDER - 2, p);
+		node->numTree = N / 2+ 1;
+		p->branch[0] = newNode;
+		*m = node->branch[N/2];
+		*node2 = p;
+		return;
+	}
+	if(pos < N / 2) {
+		copy(node, N/2, ORDER-2, p);
+		node->numTree = N/2 + 1;
+		*m = node->branch[N/2-1];
+		insNode(node, n, newNode, pos);
+		return;
+	}
+}
+int NodeSearch(NodePtr node, int m) {
 }
 void insert(NodePtr node, int k, int pos){
 	NodePtr n1, n2 f, nNewNode;
@@ -40,9 +72,44 @@ void insert(NodePtr node, int k, int pos){
 		insNode(n1, new_key, nNewNode, p);
 		return ;
 	}
-
+	split(n1, new_key, nNewNode, p, &n2, &mid_key);
+	root->branch[0] = n1;
+	root->branch[1] = n2;
+	printf("Noi dung cua khoa moi:");
+	scanf("%d", &k);
+	if(root == nullptr)
+		root->makeRoot(k);
+	else {
+		s = search(k, &p, &tim);
+		if(tim) 
+			printf("Bi trung khoa, khong them\n");
+		else 
+			insert(s, k, p);
+	}
 }
 
 int main() {
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
